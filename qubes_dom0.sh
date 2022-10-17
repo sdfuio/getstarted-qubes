33e78b73402609cb149d73de61d60180116ae106
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-17 06:54:04 -0400

    Add files via upload

---
+++ qubes_dom0.sh
@@ -0,0 +1,25 @@
+
+# untrusted qube
+git clone https://github.com/sdfuio/first-shellscript
+
+# dom0
+qvm-run --pass-io untrusted 'cat /home/user/first-shellscript/qubes_dom0.sh' >  ~/qubes_dom0.sh
+
+###########################################
+# create debian-11-dvm (disposable template)
+###########################################
+
+# in dom0, open termial and execute the following commands.
+tmp_for_dbndvm='debian-11'
+dbndvm='debian-11-dvm'
+qvm-create --template ${tmp_for_dbndvm} --label red ${dbndvm}
+qvm-prefs ${dbndvm} template_for_dispvms True
+qvm-features ${dbndvm} appmenus-dispvm 1
+
+# dispvm and disposable template are a little different.
+# dispvm: sys-usb (VM name is persistent)
+# disposable template: fedora-34-dvm (this template creates VMs whose names are random like disp123)
+# see https://forum.qubes-os.org/t/how-can-i-create-a-new-disposable-in-4-1-debian-based/10418/5
+
+
+
