652d92cfa872fcd8292f73e04231af4a6f9fc415
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-19 04:33:21 -0400

    Update libaacs.sh

--- libaacs.sh
+++ libaacs.sh
@@ -1,6 +1,7 @@
 
-
-sudo apt-get  install libtool
+sudo apt update
+sudo apt install libtool
+sudo apt install libgcrypt
 git clone https://code.videolan.org/videolan/libaacs.git
 cd libaacs
 ./bootstrap
