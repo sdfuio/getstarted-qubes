8be41e795ad4c25cbc7b773cf18eb3d59651283e
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-19 04:48:07 -0400

    Update libaacs.sh

--- libaacs.sh
+++ libaacs.sh
@@ -1,5 +1,9 @@
 
 sudo apt update
+
+sudo apt-get install build-essential
+sudo apt-get install autoconf
+sudo apt-get install -y gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 mingw-w64-tools
 sudo apt install libtool -y
 sudo apt-get install -y libgcrypt-dev
 sudo apt-get install bison -y
