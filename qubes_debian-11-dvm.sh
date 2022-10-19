16e945456fa03fa51fdb17fa60bc9ef343f17ed4
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-19 05:46:49 -0400

    Update qubes_debian-11-dvm.sh

--- qubes_debian-11-dvm.sh
+++ qubes_debian-11-dvm.sh
@@ -6,11 +6,20 @@
 im-config
 ibus-setup
 
-
 # firefox saved logins
 firefox about:logins
 # edit username and password
 
+
+###################
+# hosts
+###################
+
+# launch untrusted vm
+# curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O hosts
+# qvm-copy-to-vm debian-11-dvm hosts
+
+cp ~/QubesIncoming/untrusted/hosts /etc/hosts
 
 
 
