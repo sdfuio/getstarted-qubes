b3f8ea4690438e14ccb4ff58388490f57468416c
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-19 04:43:38 -0400

    Update libaacs.sh

--- libaacs.sh
+++ libaacs.sh
@@ -4,6 +4,7 @@ sudo apt install libtool -y
 sudo apt-get install -y libgcrypt-dev
 sudo apt-get install bison -y
 sudo apt-get install byacc -y
+sudo apt-get install flex -y
 
 git clone https://code.videolan.org/videolan/libaacs.git
 cd libaacs
