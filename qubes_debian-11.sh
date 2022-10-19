6f0b682a8962f6a01e64fd03510c294433abd6dc
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-19 04:16:59 -0400

    Update qubes_debian-11.sh

--- qubes_debian-11.sh
+++ qubes_debian-11.sh
@@ -1,9 +1,12 @@
 
 # c compiler 
-apt-get install build-essential
+sudo apt-get install build-essential
 
 # Automake
 sudo apt-get install autoconf
+
+#
+sudo apt-get install -y gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 mingw-w64-tools
 
 # dictionary
 sudo apt install dict goldendict 
