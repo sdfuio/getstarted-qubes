523aac6f1a87e47018bfa48100f85b718d5697e7
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-20 09:10:37 -0400

    Update qubes_dom0.sh

--- qubes_dom0.sh
+++ qubes_dom0.sh
@@ -31,7 +31,17 @@ qvm-copy-to-vm personal Pictures/Screenshot*.png
 
 
 
+#############################################
+# create windows qube
+#============================================
 
+# https://github.com/elliotkillick/qvm-create-windows-qube
+
+# untrusted 
+# wget https://raw.githubusercontent.com/elliotkillick/qvm-create-windows-qube/master/install.sh
+
+qvm-run -p --filter-escape-chars --no-color-output untrusted "cat '/home/user/Downloads/install.sh'" > install.sh
+chmod +x install.sh && ./install.sh
 
 
 
