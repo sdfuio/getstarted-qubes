6ef263949ee4e8e4af89ffb9d47c1ccde146c3fb
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-21 01:39:14 -0400

    Create qubes_dom0_create_windows-mgmt.sh

---
+++ qubes_dom0_create_windows-mgmt.sh
@@ -0,0 +1,15 @@
+#############################################
+# create windows-mgmt
+#============================================
+
+# https://github.com/elliotkillick/qvm-create-windows-qube
+
+qvm-run --pass-io untrusted 'wget https://raw.githubusercontent.com/elliotkillick/qvm-create-windows-qube/master/install.sh' 
+qvm-run -p --filter-escape-chars --no-color-output untrusted "cat '/home/user/Downloads/install.sh'" > install.sh
+chmod +x install.sh && ./install.sh
+
+
+
+
+
+
