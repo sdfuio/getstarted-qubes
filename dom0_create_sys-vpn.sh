58fc726cb166cb43ea527c8e43e0fd675400f018
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-22 20:53:51 -0400

    Update dom0_create_sys-vpn.sh

--- dom0_create_sys-vpn.sh
+++ dom0_create_sys-vpn.sh
@@ -1,2 +1,6 @@
 
 https://www.reddit.com/r/Qubes/comments/o3pykx/guide_how_to_setup_protonvpn_in_qubes/
+
+qvm-run -u root fedora-34-minimal-sys-vpn xterm 
+mv ~/temp/etc/yum.repos.d/protonvpn-stable-f33.repo /etc/yum.repos.d/
+
