c3a150defe6ef200e22dc09b34b95e6cdda50473
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-23 22:20:23 -0400

    Update template_debian-11.sh

--- template_debian-11.sh
+++ template_debian-11.sh
@@ -1,26 +1,33 @@
 
+#######################
+# development
+#=========================
+
 # c compiler 
 sudo apt-get install -y build-essential
-
-# Automake
-sudo apt-get install -y autoconf
 
 #
 sudo apt-get install -y gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 mingw-w64-tools
 
+# Automake
+sudo apt-get install -y autoconf
+
+
+
 # dictionary
 sudo apt install dict goldendict 
+
 
 # media player
 sudo apt install vlc
 
-#
+# graphics
+sudo apt install gimp inkscape
+
+
+# 
 sudo apt install -y libtool
-
-#
 sudo apt-get install -y libgcrypt-dev
-
-#
 sudo apt-get install -y bison
 sudo apt-get install -y byacc
 sudo apt-get install -y flex
