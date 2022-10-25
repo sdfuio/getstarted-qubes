314d684f74392ab789e3168bc0c67ec74226adcb
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-25 08:12:09 -0400

    Create dom0_personal_prefs.sh

---
+++ dom0_personal_prefs.sh
@@ -0,0 +1,8 @@
+
+qvm-prefs  personal netvm none
+qvm-prefs  personal memory 2000
+qvm-prefs  personal maxmem 6000
+qvm-prefs  personal template fedora-34
+qvm-prefs  personal default_dispvm fedora-34-dvm
+
+
