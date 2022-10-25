3be4e5d60a06c1c436ce13ac01a5672f1bc5c0e6
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-25 08:04:18 -0400

    Update dom0_install_template.sh

--- dom0_install_template.sh
+++ dom0_install_template.sh
@@ -1,6 +1,9 @@
 
-# https://www.qubes-os.org/doc/templates/minimal/
+# install minimam templates: https://www.qubes-os.org/doc/templates/minimal/
 sudo qubes-dom0-update qubes-template-fedora-34-minimal
 sudo qubes-dom0-update qubes-template-debian-11-minimal
+
+# install new template
+sudo qubes-dom0-update qubes-template-fedora-36
 
 
