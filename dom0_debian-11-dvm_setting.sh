f8132b71a2842fa86d76fe376f77d276673fb838
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-25 08:43:05 -0400

    Update dom0_debian-11-dvm_setting.sh

--- dom0_debian-11-dvm_setting.sh
+++ dom0_debian-11-dvm_setting.sh
@@ -1,1 +1,11 @@
 qvm-volume extend debian-11-dvm:private 10G
+
+qvm-run debian-10-dvm vlc # initial setting 
+
+qvm-run debian-10-dvm gnome-terminal
+# debian-10-dvm 
+# im-config # select ibus as the defalut IME
+# ibus-setup # add japanese
+# qvm-shutdown debian-10-dvm
+
+
