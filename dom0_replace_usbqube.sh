c68ea98a10a09eb88ffa04dd69a7c64fa0d194f8
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-30 19:07:55 +0900

    Update dom0_replace_usbqube.sh

--- dom0_replace_usbqube.sh
+++ dom0_replace_usbqube.sh
@@ -11,19 +11,21 @@ oldusb='sys-usb'
 # attach USB controller to new usb qube
 usb_bdf=$(qvm-pci | awk '/USB *controller/ {print $1}') # mine is 'dom0:00_14.0' 
 qvm-pci attach --persistent ${newusb} ${usb_bdf} -o no-strict-reset=true
+
+# new usb qube will start automatically
 qvm-prefs ${newusb} autostart true
 
 # edit qubes.InputMouse
 sudo sed -i 's/^sys-usb/^sys-usb1/g'  /etc/qubes-rpc/policy/qubes.InputMouse
 
-# 
+# old usb qube will not start automatically
 qvm-prefs ${oldusb} autostart false
 
 # detach USB controller from old USB qube
-qvm-shutdown ${oldusb} && read -p "if you already unplug all cables from USB ports, press enter: "
+qvm-shutdown ${oldusb} && read -p "if you have already unpluged all cables from USB ports, press enter: "
 qvm-pci detach ${oldusb} ${usb_bdf}
 
-# reboot or logout
+echo  "reboot or logout"
 
 
 
