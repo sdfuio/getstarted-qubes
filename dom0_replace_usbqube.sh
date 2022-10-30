6af51a431ce0b22185b52f104417c6ab98f15d2a
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-30 18:50:28 +0900

    Update dom0_replace_usbqube.sh

--- dom0_replace_usbqube.sh
+++ dom0_replace_usbqube.sh
@@ -1,9 +1,11 @@
 #######################################
-# Use disp-sys-usb instead of sys-usb
+# Use sys-usb1 instead of sys-usb
 #======================================
 
-usbtmp='fedora-36-dvm'
-newusb='disp-sys-usb'
+
+# unplug all cables from USB ports to prevent attaching an infected device to dom0 directly.
+
+newusb='sys-usb1'
 oldusb='sys-usb'
 
 # attach USB controller to new usb qube
@@ -12,20 +14,17 @@ qvm-pci attach --persistent ${newusb} ${usb_bdf} -o no-strict-reset=true
 qvm-prefs ${newusb} autostart true
 
 # edit qubes.InputMouse
-sudo sed -i 's/^sys-usb/disp-sys-usb/g'  /etc/qubes-rpc/policy/qubes.InputMouse
+sudo sed -i 's/^sys-usb/^sys-usb1/g'  /etc/qubes-rpc/policy/qubes.InputMouse
 
 # 
 qvm-prefs ${oldusb} autostart false
 
 # detach old USB qube from USB controller
 qvm-shutdown ${oldusb}
-read -p "Wait until the shutdown has completed, and press enter."
+read -p "if you already unplug all cables from USB ports, press enter: "
 qvm-pci detach ${oldusb} ${usb_bdf}
 
-# start new USB qube
-qvm-start ${newusb}
-
-
+# reboot or logout
 
 
 
