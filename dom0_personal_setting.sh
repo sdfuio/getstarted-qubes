a7c06e677a7da21a09f1d227af8467b59a8a5e55
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-10-31 22:41:14 -0400

    Update dom0_personal_setting.sh

--- dom0_personal_setting.sh
+++ dom0_personal_setting.sh
@@ -1,8 +1,22 @@
+##########################
+# Qubes setting
+#=======================
 
+
+# personal
 qvm-prefs  personal netvm none
 qvm-prefs  personal memory 2000
 qvm-prefs  personal maxmem 6000
 qvm-prefs  personal template fedora-34
 qvm-prefs  personal default_dispvm fedora-34-dvm
 qvm-volume extend personal:private 10G
+
+
+# work
+qvm-prefs work netvm none
+qvm-volume extend personal:private 5G
+
+# sys-usb
+qvm-volume extend sys-usb:private 10G
+qvm-prefs  personal netvm none # default
 
