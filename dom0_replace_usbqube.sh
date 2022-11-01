e7d7d5ddf1448148ab8599cc1ad655bbd868ef22
sdfuio <114386583+sdfuio@users.noreply.github.com>
2022-11-01 01:43:08 -0400

    Update dom0_replace_usbqube.sh

--- dom0_replace_usbqube.sh
+++ dom0_replace_usbqube.sh
@@ -16,7 +16,7 @@ qvm-pci attach --persistent ${newusb} ${usb_bdf}
 qvm-prefs ${newusb} autostart true
 
 # edit qubes.InputMouse
-sudo sed -i 's/^sys-usb/sys-usb1/g'  /etc/qubes-rpc/policy/qubes.InputMouse
+sudo sed -i 's/^sys-usb /sys-usb1 /g'  /etc/qubes-rpc/policy/qubes.InputMouse
 
 # old usb qube will not start automatically
 qvm-prefs ${oldusb} autostart false
